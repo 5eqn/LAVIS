6fbe9fe4eaf4242d0a6ead4494aa119ce792c03c
5eqn <491100866@qq.com>
2024-08-29 10:50:22 +0800

    generalize over GPU count

--- run_scripts/instructblip/eval_vqav2.sh
+++ run_scripts/instructblip/eval_vqav2.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.run --nproc_per_node=8 evaluate.py --cfg-path lavis/projects/instructblip/vqa_test.yaml
+python -m torch.distributed.run --nproc_per_node=$GPU_COUNT evaluate.py --cfg-path lavis/projects/instructblip/vqa_test.yaml
